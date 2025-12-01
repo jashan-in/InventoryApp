09ef9acec73cd23d6e1acc4fbe61e69d5d8ecf92
Jashanpreet Singh <jashanpreetsingh11@rrc.ca>
2025-12-01 15:00:24 -0600

    Fixed

--- lambda/get_inventory_item/lambda_function.py
+++ lambda/get_inventory_item/lambda_function.py
@@ -1,20 +1,21 @@
 import json
 import boto3
-import json
 from decimal import Decimal
 
 def decimal_converter(o):
     if isinstance(o, Decimal):
         return float(o)
-
+    raise TypeError
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("Inventory")
 
 def lambda_handler(event, context):
     try:
+        # Read path parameter ID
         item_id = event["pathParameters"]["id"]
 
+        # Query DynamoDB
         response = table.get_item(Key={"item_id": item_id})
 
         if "Item" not in response:
@@ -23,9 +24,12 @@ def lambda_handler(event, context):
                 "body": json.dumps({"error": "Item not found"})
             }
 
+        # Convert DynamoDB Decimal values → float
+        item = json.loads(json.dumps(response["Item"], default=decimal_converter))
+
         return {
             "statusCode": 200,
-            "body": json.dumps(response["Item"])
+            "body": json.dumps(item)
         }
 
     except Exception as e:
